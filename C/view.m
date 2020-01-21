ba33d80c10f6916e4edbd622d1503c2064cefa73
byulparan <byulparan@gmail.com>
2020-01-22 05:29:35 +0900

    add `view.m` for 2D

--- C/view.m
+++ C/view.m
@@ -1,17 +1,77 @@
+#import "view.h"
 #import <Cocoa/Cocoa.h>
 
 @interface LispView : NSView {
   int mID;
-  void (*mDrawFn) (int);
+  DrawFn mDrawFn;
+  MouseFn mMouseFn;
 }
 @end
 
 @implementation LispView
--(id) initWithID: (int) inID X: (int) x Y: (int) y W: (int) w H: (int) h closeFn: (void(*)(int)) inDrawFn {
-  NSRect frame = NSMakeRect(x, y, w, h);
+
+-(id) initWithID: (int) inID
+	   frame: (NSRect) frame
+	  drawFn: (DrawFn) drawFn
+	 mouseFn: (MouseFn) mouseFn 
+{
+  self = [super initWithFrame: frame];
   mID = inID;
-  mDrawFn = inDrawFn;
+  mDrawFn = drawFn;
+  mMouseFn = mouseFn;
   return self;
 }
 
+-(void) drawRect:(NSRect) frame {
+  mDrawFn(mID, DRAW, NULL, NULL, frame.size.width, frame.size.height);
+}
+
+-(void) dealloc {
+  [super dealloc];
+}
+
+-(void) mouseDown:(NSEvent*) event {
+  NSPoint point = [self convertPoint: [event locationInWindow]
+			    fromView: nil];
+  mMouseFn(mID, DOWN, event, point.x, point.y);
+}
+
+-(void) mouseDragged:(NSEvent*) event {
+  NSPoint point = [self convertPoint: [event locationInWindow]
+			    fromView: nil];
+  mMouseFn(mID, DRAGGED, event, point.x, point.y);
+}
+
+-(void) mouseUp:(NSEvent*) event {
+  NSPoint point = [self convertPoint: [event locationInWindow]
+			    fromView: nil];
+  mMouseFn(mID, UP, event, point.x, point.y);
+}
+
+-(void) mouseMoved: (NSEvent*) event {
+  NSPoint point = [self convertPoint: [event locationInWindow]
+			    fromView: nil];
+  mMouseFn(mID, MOVED, event, point.x, point.y);
+}
+
+
+-(void) scrollWheel:(NSEvent*) event {
+  NSPoint point = [self convertPoint: [event locationInWindow]
+			    fromView: nil];
+  mMouseFn(mID, SCROLLWHEEL, event, point.x, point.y);
+}
+
 @end
+
+// ============================================================
+// export C
+// ============================================================
+
+LispView* make_view(int inID, bool isAnimate, int x, int y, int w, int h,
+		    DrawFn drawFn, MouseFn mouseFn) {
+  LispView* view = [[LispView alloc] initWithID: inID
+					  frame: NSMakeRect(x, y ,w, h)
+					 drawFn: drawFn
+					mouseFn: mouseFn];
+  return view;
+}
