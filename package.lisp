b5498a96ba1f90ecb58bbfa9a2b92bcdac5c331c
byulparan <byul@byulparanui-MacBook-Air.local>
2020-01-25 01:10:53 +0900

    add cf-retain cf-release cf-autorelease

--- package.lisp
+++ package.lisp
@@ -2,51 +2,54 @@
   (:use :cl :alexandria)
   #+ccl (:import-from #:ccl #:make-id-map #:assign-id-map-id #:id-map-free-object)
   (:nicknames :ns)
-  (:export #:cls
-	   #:sel
-	   #:objc
-	   #:retain
-	   #:release
-	   #:autorelease
-	   #:cf-release
-	   #:make-ns-string
-	   #:ns-string-to-lisp
-	   #:make-cf-string
-	   #:cf-string-to-lisp
-
-	   #:start-event-loop 
-	   #:with-event-loop
-	   
-	   #:timer
-	   #:invalidate
-	   
-	   #:window
-	   #:window-show
-	   #:toggle-fullscreen
-	   #:content-view
-	   #:add-subviews
-
-	   #:width
-	   #:height
-	   #:cgl-context
-	   #:cgl-pixel-format
-	   #:init
-	   #:draw
-	   #:shutdown
-	   #:mouse-down
-	   #:mouse-dragged
-	   #:mouse-up
-	   #:mouse-moved
-	   #:mouse-wheel
-	   #:command-p
-	   #:shift-p
-	   #:ctrl-p
-	   #:opt-p
-	   #:redisplay
-
-	   #:view
-	   #:current-cg-context
-	   
-	   #:opengl-view
-	   #:reshape))
+  (:export
+   ;; core-foundation.lisp
+   #:cls
+   #:sel
+   #:objc
+   #:retain
+   #:release
+   #:autorelease
+   #:cf-retain
+   #:cf-release
+   #:cf-autorelease
+   #:make-ns-string
+   #:ns-string-to-lisp
+   #:make-cf-string
+   #:cf-string-to-lisp
+   ;; application.lisp
+   #:start-event-loop 
+   #:with-event-loop
+   ;; timer.lisp
+   #:timer
+   #:invalidate
+   ;; window.lisp
+   #:window
+   #:window-show
+   #:toggle-fullscreen
+   #:content-view
+   #:add-subviews
+   ;; view.lisp
+   #:width
+   #:height
+   #:cgl-context
+   #:cgl-pixel-format
+   #:init
+   #:draw
+   #:shutdown
+   #:mouse-down
+   #:mouse-dragged
+   #:mouse-up
+   #:mouse-moved
+   #:mouse-wheel
+   #:command-p
+   #:shift-p
+   #:ctrl-p
+   #:opt-p
+   #:redisplay
+   #:view
+   #:current-cg-context
+   ;; opengl-view.lisp
+   #:opengl-view
+   #:reshape))
 
