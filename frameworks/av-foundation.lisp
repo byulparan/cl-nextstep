34c9b04967eb152bc268a905c786d16a1109e88e
byulparan <byulparan@gmail.com>
2020-01-29 00:29:32 +0900

    add av:with-media-data for av:capture / av:player

--- frameworks/av-foundation.lisp
+++ frameworks/av-foundation.lisp
@@ -1,21 +1,36 @@
 (defpackage :av
   (:use :cl)
-  (:export #:list-camera-device
+  (:export #:with-media-data
+	   #:list-camera-device
 	   #:capture
 	   #:make-camera-capture
 	   #:start-capture
 	   #:stop-capture
 	   #:release-capture
-	   #:with-capture-data
 
 	   #:player
 	   #:make-player
 	   #:status
 	   #:play
 	   #:pause
-	   #:volume))
+	   #:volume
+	   #:release-player))
 
 (in-package :av)
+
+(defgeneric get-delegate (av-media))
+
+(defmacro with-media-data ((av-media bpr width height data) &body body)
+  (alexandria:with-gensyms (m-head)
+    `(let* ((,m-head (ns:objc (get-delegate ,av-media) "getImageBuffer" :pointer)))
+       (unless (cffi:null-pointer-p ,m-head)
+	 (cffi:foreign-funcall "CVPixelBufferLockBaseAddress" :pointer ,m-head :int 0)
+	 (unwind-protect (let* ((,bpr (cffi:foreign-funcall "CVPixelBufferGetBytesPerRow" :pointer ,m-head :sizet))
+				(,width (cffi:foreign-funcall "CVPixelBufferGetWidth" :pointer ,m-head :sizet))
+				(,height (cffi:foreign-funcall "CVPixelBufferGetHeight" :pointer ,m-head :sizet))
+				(,data (cffi:foreign-funcall "CVPixelBufferGetBaseAddress" :pointer ,m-head :pointer)))
+			   ,@body)
+	   (cffi:foreign-funcall "CVPixelBufferUnlockBaseAddress" :pointer ,m-head :int 0))))))
 
 ;; Capture
 (defun list-camera-device ()
@@ -42,10 +57,10 @@
 	  (assert (zerop code) nil "Error while make camera capture: ~a" code)
 	  input)))))
 
-(defun make-capture-video-data-output (output-delegate)
+(defun make-capture-video-data-output (capture-delegate)
   (let* ((video-output (ns:autorelease (ns:objc (ns:alloc "AVCaptureVideoDataOutput") "init" :pointer))))
     (ns:objc video-output "setSampleBufferDelegate:queue:"
-	     :pointer output-delegate :pointer (cffi:foreign-symbol-pointer "_dispatch_main_q"))
+	     :pointer capture-delegate :pointer (cffi:foreign-symbol-pointer "_dispatch_main_q"))
     (ns:objc video-output "setVideoSettings:"
 	     :pointer (ns:objc "NSDictionary" "dictionaryWithObject:forKey:"
 			       :pointer (ns:objc "NSNumber" "numberWithInt:" :int #x00000020 ;; kCVPixelFormatType_32ARGB
@@ -56,15 +71,18 @@
     video-output))
 
 (defstruct capture
-  session data)
+  session delegate)
+
+(defmethod get-delegate ((av-media capture))
+  (capture-delegate av-media))
 
 (defun make-capture-device (input)
   (let* ((session (ns:objc (ns:alloc "AVCaptureSession") "init" :pointer))
-	 (output-delegate (ns:objc (ns:alloc "CaptureVideoDataOutputDelegate") "init" :pointer))
-	 (output (make-capture-video-data-output output-delegate)))
+	 (capture-delegate (ns:objc (ns:alloc "CaptureDelegate") "init" :pointer))
+	 (output (make-capture-video-data-output capture-delegate)))
     (ns:objc session "addInput:" :pointer input)
     (ns:objc session "addOutput:" :pointer output)
-    (make-capture :session session :data output-delegate)))
+    (make-capture :session session :delegate capture-delegate)))
 
 (defun make-camera-capture (index)
   (ns:with-event-loop (:waitp t)
@@ -81,19 +99,7 @@
 (defun release-capture (capture)
   (ns:with-event-loop (:waitp t)
     (ns:release (capture-session capture))
-    (ns:release (capture-data capture))))
-
-(defmacro with-capture-data ((capture bpr width height data) &body body)
-  (alexandria:with-gensyms (m-head)
-    `(let* ((,m-head (ns:objc (capture-data ,capture) "getImageBuffer" :pointer)))
-       (cffi:foreign-funcall "CVPixelBufferLockBaseAddress" :pointer ,m-head :int 0)
-       (unwind-protect (let* ((,bpr (cffi:foreign-funcall "CVPixelBufferGetBytesPerRow" :pointer ,m-head :sizet))
-			      (,width (cffi:foreign-funcall "CVPixelBufferGetWidth" :pointer ,m-head :sizet))
-			      (,height (cffi:foreign-funcall "CVPixelBufferGetHeight" :pointer ,m-head :sizet))
-			      (,data (cffi:foreign-funcall "CVPixelBufferGetBaseAddress" :pointer ,m-head :pointer)))
-			 ,@body)
-	 (cffi:foreign-funcall "CVPixelBufferUnlockBaseAddress" :pointer ,m-head :int 0)))))
-
+    (ns:release (capture-delegate capture))))
 
 
 ;; Player
@@ -103,42 +109,46 @@
   ((id :accessor id)
    (g-id :initform 0 :accessor g-id :allocation :class)
    (player :accessor player)
+   (player-item :accessor player-item)
    (player-delegate :accessor player-delegate)
-   (did-end :initform nil :accessor did-end)))
+   (did-end :initarg :did-end :initform nil :accessor did-end)))
+
+(defmethod get-delegate ((av-media player))
+  (player-delegate av-media))
 
 (cffi:defcallback player-did-reach-end :void ((id :int))
   (alexandria:when-let* ((player (gethash id *player-table*))
 			 (did-end (did-end player)))
     (funcall did-end)))
 
-
 (defmethod initialize-instance :after ((self player) &key did-end)
   (setf (id self) (g-id self))
   (incf (g-id self))
   (setf (gethash (id self) *player-table*) self))
 
-(defun make-player (path)
+(defun make-player (path &key did-end)
   (let* ((path (uiop:truenamize path)))
     (assert (probe-file path) nil "can't find file: ~a" path)
     (ns:with-event-loop (:waitp t)
-      (let* ((player (make-instance 'player))
+      (let* ((player (make-instance 'player :did-end did-end))
 	     (av-player (ns:objc (ns:alloc "AVPlayer")
 				 "initWithURL:" :pointer (ns:objc "NSURL" "fileURLWithPath:"
 								  :pointer (ns:autorelease (ns:make-ns-string (namestring path)))
 								  :pointer)
 				 :pointer))
 	     (av-player-item (ns:objc av-player "currentItem" :pointer))
-	     (player-item-delegate (ns:objc (ns:alloc "PlayerItemDelegate")
+	     (player-delegate (ns:objc (ns:alloc "PlayerDelegate")
 	     				    "initWithID:endFn:" :int (id player)
 	     				    :pointer (cffi:callback player-did-reach-end)
 	     				    :pointer)))
 	(ns:objc av-player-item "addObserver:forKeyPath:options:context:"
-		 :pointer player-item-delegate
+		 :pointer player-delegate
 		 :pointer (ns:autorelease (ns:make-ns-string "status"))
 		 :int 0
 		 :pointer (cffi:null-pointer))
 	(setf (player player) av-player
-	      (player-delegate player) player-item-delegate)
+	      (player-item player) av-player-item
+	      (player-delegate player) player-delegate)
 	player))))
 
 (defun status (player)
@@ -160,6 +170,10 @@
 (defun volume (player volume)
   (ns:with-event-loop nil
     (ns:objc (player player) "setVolume:" :float (float volume 1.0))))
+
+(defun release-player (player)
+  (ns:release (player player))
+  (ns:release (player-delegate player)))
 
 
 
