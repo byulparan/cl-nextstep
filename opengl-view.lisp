a9ed6c092e4f75322fd47c4e34f183ef71fcdb96
byulparan <byulparan@gmail.com>
2020-01-30 01:20:48 +0900

    destroy cgl-pixel-format after create opengl-view.

--- opengl-view.lisp
+++ opengl-view.lisp
@@ -10,15 +10,18 @@
 
 
 (defmethod initialize-instance :after ((self opengl-view) &key (x 0) (y 0) (w 400) (h 200))
-  (setf (cocoa-ref self) (ns:objc
-			  (ns:objc "LispOpenGLView" "alloc" :pointer)
-			  "initWithID:frame:pixelFormat:isAnimate:drawFn:mouseFn:"
-			  :int (id self)
-			  (:struct ns:rect) (ns:make-rect x y w h)
-			  :pointer (cgl:make-pixel-format (cgl:make-attributes
-							   :core-profile (core-profile self)))
-			  :unsigned-char 1
-			  :pointer (cffi:callback draw-callback)
-			  :pointer (cffi:callback mouse-callback)
-			  :pointer)))
+  (let* ((pixel-format (cgl:make-pixel-format (cgl:make-attributes
+					       :core-profile (core-profile self)))))
+    (unwind-protect
+	 (setf (cocoa-ref self) (ns:objc
+				 (ns:objc "LispOpenGLView" "alloc" :pointer)
+				 "initWithID:frame:pixelFormat:isAnimate:drawFn:mouseFn:"
+				 :int (id self)
+				 (:struct ns:rect) (ns:make-rect x y w h)
+				 :pointer pixel-format
+				 :unsigned-char 1
+				 :pointer (cffi:callback draw-callback)
+				 :pointer (cffi:callback mouse-callback)
+				 :pointer))
+      (cgl:destroy-pixel-format pixel-format))))
 
