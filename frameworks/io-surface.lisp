3a65f50aa6ebc2f46d1ea68670e82a73408fe8d7
byulparan <byulparan@gmail.com>
2020-02-08 00:57:24 +0900

    fix encode/decode of pixel-format-type.

--- frameworks/io-surface.lisp
+++ frameworks/io-surface.lisp
@@ -19,44 +19,49 @@
 (in-package :io-surface)
 
 (defun make-surface (width height &optional (pixel-format "ARGB"))
-  (let* ((dictionary (cffi:foreign-funcall "CFDictionaryCreateMutable"
-					   :pointer (cffi:null-pointer)
-					   :long 0
-					   :pointer (cffi:foreign-symbol-pointer "kCFTypeDictionaryKeyCallBacks")
-					   :pointer (cffi:foreign-symbol-pointer "kCFTypeDictionaryValueCallBacks")
-					   :pointer))
-	 (bytes-per-element 4)
-	 (kCFNumberSInt32Type 3))
-    (cffi:with-foreign-objects ((%bytes-per-row :int) 
-				(%bytes-per-element :int)
-				(%width :int)
-				(%height :int)
-				(%pixel-format :char 4)
-				(%size :int))
-      (setf (cffi:mem-ref %bytes-per-row :int) (* width bytes-per-element)
-	    (cffi:mem-ref %bytes-per-element :int) bytes-per-element
-	    (cffi:mem-ref %width :int) width
-	    (cffi:mem-ref %height :int) height
-	    (cffi:mem-ref %size :int) (* width height bytes-per-element))
-      (loop for pix in (map 'list #'char-code pixel-format)
-	    for i from 0 do (setf (cffi:mem-aref %pixel-format :char i) pix))
-      (macrolet ((cf-dict-set (key value)
-		   `(cffi:foreign-funcall "CFDictionarySetValue"
-					  :pointer dictionary
-					  :pointer (cffi:mem-ref (cffi:foreign-symbol-pointer ,key) :pointer)
-					  :pointer (ns:cf-autorelease
-						    (cffi:foreign-funcall "CFNumberCreate"
-									  :pointer (cffi:null-pointer)
-									  :long kCFNumberSInt32Type
-									  :pointer ,value
-									  :pointer)))))
-	(cf-dict-set "kIOSurfaceBytesPerRow" %bytes-per-row)
-	(cf-dict-set "kIOSurfaceBytesPerElement" %bytes-per-element)
-	(cf-dict-set "kIOSurfaceWidth" %width)
-	(cf-dict-set "kIOSurfaceHeight" %height)
-	(cf-dict-set "kIOSurfacePixelFormat" %pixel-format)
-	(cf-dict-set "kIOSurfaceAllocSize" %size)
-	(cffi:foreign-funcall "IOSurfaceCreate" :pointer dictionary :pointer)))))
+  (flet ((encode-type (type)
+	   (let ((codes (map 'list #'char-code type)))
+	     (+ (ash (nth 0 codes) 24)
+		(ash (nth 1 codes) 16)
+		(ash (nth 2 codes)  8)
+		(nth 3 codes)))))
+    (let* ((dictionary (cffi:foreign-funcall "CFDictionaryCreateMutable"
+					     :pointer (cffi:null-pointer)
+					     :long 0
+					     :pointer (cffi:foreign-symbol-pointer "kCFTypeDictionaryKeyCallBacks")
+					     :pointer (cffi:foreign-symbol-pointer "kCFTypeDictionaryValueCallBacks")
+					     :pointer))
+	   (bytes-per-element 4)
+	   (kCFNumberSInt32Type 3))
+      (cffi:with-foreign-objects ((%bytes-per-row :int) 
+				  (%bytes-per-element :int)
+				  (%width :int)
+				  (%height :int)
+				  (%pixel-format :int)
+				  (%size :int))
+	(setf (cffi:mem-ref %bytes-per-row :int) (* width bytes-per-element)
+	      (cffi:mem-ref %bytes-per-element :int) bytes-per-element
+	      (cffi:mem-ref %width :int) width
+	      (cffi:mem-ref %height :int) height
+	      (cffi:mem-ref %pixel-format :int) (encode-type pixel-format)
+	      (cffi:mem-ref %size :int) (* width height bytes-per-element))
+	(macrolet ((cf-dict-set (key value)
+		     `(cffi:foreign-funcall "CFDictionarySetValue"
+					    :pointer dictionary
+					    :pointer (cffi:mem-ref (cffi:foreign-symbol-pointer ,key) :pointer)
+					    :pointer (ns:cf-autorelease
+						      (cffi:foreign-funcall "CFNumberCreate"
+									    :pointer (cffi:null-pointer)
+									    :long kCFNumberSInt32Type
+									    :pointer ,value
+									    :pointer)))))
+	  (cf-dict-set "kIOSurfaceBytesPerRow" %bytes-per-row)
+	  (cf-dict-set "kIOSurfaceBytesPerElement" %bytes-per-element)
+	  (cf-dict-set "kIOSurfaceWidth" %width)
+	  (cf-dict-set "kIOSurfaceHeight" %height)
+	  (cf-dict-set "kIOSurfacePixelFormat" %pixel-format)
+	  (cf-dict-set "kIOSurfaceAllocSize" %size)
+	  (cffi:foreign-funcall "IOSurfaceCreate" :pointer dictionary :pointer))))))
 
 (cffi:defcfun ("IOSurfaceDecrementUseCount" decrement-use-count) :void
   (buffer :pointer))
@@ -80,13 +85,15 @@
   (buffer :pointer))
 
 (defun pixel-format (buffer)
-  (let* ((os-type (cffi:foreign-funcall "IOSurfaceGetPixelFormat"
-					:pointer buffer
-					:unsigned-int)))
-    (map 'string #'code-char (list (logand os-type #xff)
-				   (logand (ash os-type -8) #xff)
-				   (logand (ash os-type -16) #xff)
-				   (ash os-type -24)))))
+  (flet ((decode-type (type)
+	   (map 'string #'code-char (list (ash type -24)
+					  (logand (ash type -16) #xff)
+					  (logand (ash type -8) #xff)
+					  (logand type #xff)))))
+    (let* ((os-type (cffi:foreign-funcall "IOSurfaceGetPixelFormat"
+					  :pointer buffer
+					  :unsigned-int)))
+      (decode-type os-type))))
 
 (cffi:defcfun ("IOSurfaceGetPlaneCount" plane-count) :sizet
   (buffer :pointer))
