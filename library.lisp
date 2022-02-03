d12aaa1b36334a53ad75c946886329d164a6208c
Parksungmin <byulparan@gmail.com>
2022-02-03 14:03:10 +0900

    rename build lib to libcl-nextstep.dylib

--- library.lisp
+++ library.lisp
@@ -1,7 +1,7 @@
 (in-package :cl-nextstep)
 
 (cffi:define-foreign-library lib-nextstep
-  (:darwin "libcl_nextstep.dylib"))
+  (:darwin "libcl-nextstep.dylib"))
 
 (cffi:use-foreign-library lib-nextstep)
 
