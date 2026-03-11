1def0a4ebd76d5c8e13f674a1601da01e28c42cd
Sungmin Park <byulparan@gmail.com>
2026-03-11 19:59:20 +0900

    Apply pass/return struct by value for widget

--- widget.lisp
+++ widget.lisp
@@ -4,8 +4,15 @@
   ((cocoa-ref :accessor cocoa-ref)))
 
 (defmethod initialize-instance :after ((self text-field) &key (x 0) (y 0) (w 10) (h 10) editable text back-color fore-color)
-  (let* ((ref (objc (alloc "NSTextField") "initWithFrame:" (:struct rect) (rect x y w h)
-		    :pointer)))
+  (let* ((ref (with-sb-alien-rect (rect (rect x y w h))
+		(sb-alien:alien-funcall
+		 (sb-alien:extern-alien "objc_msgSend" (sb-alien:function sb-alien:system-area-pointer
+									  sb-alien:system-area-pointer
+									  sb-alien:system-area-pointer
+									  (sb-alien:struct rect)))
+		 (alloc "NSTextField")
+		 (sel "initWithFrame:")
+		 rect))))
     (objc ref "setEditable:" :bool editable)
     (when back-color (objc ref "setBackgroundColor:" :pointer back-color))
     (when fore-color (objc ref "setTextColor:" :pointer fore-color))
@@ -18,8 +25,15 @@
 
 
 (defmethod initialize-instance :after ((self slider) &key (x 0) (y 0) (w 10) (h 10) action value color)
-  (let* ((ref (objc (alloc "NSSlider") "initWithFrame:" (:struct rect) (rect x y w h)
-		    :pointer)))
+  (let* ((ref (with-sb-alien-rect (rect (rect x y w h))
+		(sb-alien:alien-funcall
+		 (sb-alien:extern-alien "objc_msgSend" (sb-alien:function sb-alien:system-area-pointer
+									  sb-alien:system-area-pointer
+									  sb-alien:system-area-pointer
+									  (sb-alien:struct rect)))
+		 (alloc "NSSlider")
+		 (sel "initWithFrame:")
+		 rect))))
     (setf (gethash (cffi:pointer-address ref) *user-action-table*) action)
     (when value
       (objc ref "setFloatValue:" :float (float value 1.0)))
